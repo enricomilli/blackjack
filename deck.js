08ff3f8fd0e18d9c39b6eb8d75023552185138e7
enricomilli <enricomilli666@gmail.com>
2024-08-24 11:26:50 +0200

    feat: moved deck into deck.js

---
+++ deck.js
@@ -0,0 +1,72 @@
+export default blackjackdeck = [
+    {
+    name: "two",
+    cardsum: 2,
+    image: "card2.png",
+},
+{
+    name: "three",
+    cardsum: 3,
+    image: "card3.png",
+},
+{
+    name: "four",
+    cardsum: 4,
+    image: "card4.png",
+},
+{
+    name: "five",
+    cardsum: 5,
+    image: "card5.png",
+},
+{
+    name: "six",
+    cardsum: 6,
+    image: "card6.png",
+},
+{
+    name: "seven",
+    cardsum: 7,
+    image: "card7.png",
+},
+{
+    name: "eight",
+    cardsum: 8,
+    image: "card8.png",
+},
+{
+    name: "nine",
+    cardsum: 9,
+    image: "card9.png",
+},
+{
+    name: "ten",
+    cardsum: 10,
+    image: "card10.png",
+},
+{
+    name: "jack",
+    cardsum: 10,
+    image: "jack-of-hearts.png",
+},
+{
+    name: "queen",
+    cardsum: 10,
+    image: "queencard.png",
+},
+{
+    name: "king",
+    cardsum: 10,
+    image: "king-card.png",
+},
+{
+    name: "ace",
+    cardsum: 11,
+    image: "ace.png",
+},
+{
+    name: "backOfCard",
+    cardsum: 0,
+    image: "Cardback.png"
+}
+]
